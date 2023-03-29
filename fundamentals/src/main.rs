97068aa9078f40c5c44b7e7b70d72abdce8c8c05
iAmNsengi <iamnsengi@icloud.com>
2023-03-29 18:01:22 +0200

    ft: number guessing game

--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use rand::Rng;
+
 fn main() {
 //   println!("Hey what's your name?");
 //   let mut name = String::new();
@@ -8,7 +10,13 @@ fn main() {
 
 println!("Hey guess a number: ");
 let mut num = String::new();
+let correct_num  = rand::rng().random_range(1..=100);
 
 io::stdin().read_line(&mut num).expect("Error in reading input");
-println!("You guessed {}.", num.trim())
+println!("You guessed {}.", num.trim());
+println!("The correct number is {}",correct_num);
+let parsed_num : i32 = num.trim().parse().expect("Error parsing to number");
+let winner = if correct_num == parsed_num {"User"} else {"PC"};
+
+println!("The winner is {}", winner)
 }
