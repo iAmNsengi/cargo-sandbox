66482079ba3437e8934e460dd40f7f57819709df
iAmNsengi <iamnsengi@icloud.com>
2023-03-29 17:33:17 +0200

    chore: number guessing game

--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -1,8 +1,14 @@
 use std::io;
 fn main() {
-  println!("Hey what's your name?");
-  let mut name = String::new();
+//   println!("Hey what's your name?");
+//   let mut name = String::new();
 
-  io::stdin().read_line(&mut name);
-println!("Welcome {}, to our app.", name.trim())
+//   io::stdin().read_line(&mut name).expect("Error reading input");
+// println!("Welcome {}, to our app.", name.trim())
+
+println!("Hey guess a number: ");
+let mut num = String::new();
+
+io::stdin().read_line(&mut num).expect("Error in reading input");
+println!("You guessed {}.", num.trim())
 }
