847951dafc0040ea0fb0b904212db3a76f1e7630
iAmNsengi <iamnsengi@icloud.com>
2023-03-29 18:31:52 +0200

    fx: refactor use match insted of ifs to match some patters(works like switch case_

--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, cmp::Ordering};
 use rand::Rng;
 
 fn main() {
@@ -15,8 +15,15 @@ let correct_num  = rand::rng().random_range(1..=10);
 io::stdin().read_line(&mut num).expect("Error in reading input");
 println!("You guessed {}.", num.trim());
 println!("The correct number is {}",correct_num);
-let parsed_num : i32 = num.trim().parse().expect("Error parsing to number");
-let winner = if correct_num == parsed_num {String::from("User")} else {String::from("PC")};
+
+let num : i32 = num.trim().parse().expect("Error parsing to number");
+
+// let winner = if correct_num == parsed_num {String::from("User")} else {String::from("PC")};
+
+let winner = match num.cmp(&correct_num) {
+    Ordering::Equal => String::from("User"),
+    _ => String::from("PC")
+};
 
 println!("The winner is {}", winner)
 }
