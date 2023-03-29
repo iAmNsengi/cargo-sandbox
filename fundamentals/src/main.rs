fb7d79a7e3f66f0bebfc4ba6915a2cc79de0b412
iAmNsengi <iamnsengi@icloud.com>
2023-03-29 18:17:49 +0200

    fx: refactor use native String formatters

--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -8,15 +8,15 @@ fn main() {
 //   io::stdin().read_line(&mut name).expect("Error reading input");
 // println!("Welcome {}, to our app.", name.trim())
 
-println!("Hey guess a number: ");
+println!("Hey guess a number from 1-10: ");
 let mut num = String::new();
-let correct_num  = rand::rng().random_range(1..=100);
+let correct_num  = rand::rng().random_range(1..=10);
 
 io::stdin().read_line(&mut num).expect("Error in reading input");
 println!("You guessed {}.", num.trim());
 println!("The correct number is {}",correct_num);
 let parsed_num : i32 = num.trim().parse().expect("Error parsing to number");
-let winner = if correct_num == parsed_num {"User"} else {"PC"};
+let winner = if correct_num == parsed_num {String::from("User")} else {String::from("PC")};
 
 println!("The winner is {}", winner)
 }
