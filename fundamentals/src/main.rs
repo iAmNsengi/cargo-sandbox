10562318059dcffa8f8f4ba11dbc0512a0a6c534
iAmNsengi <iamnsengi@icloud.com>
2023-04-01 11:06:18 +0200

    ft: 🤗 allow user to enter number of guesses they will take

--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -8,8 +8,15 @@ fn main() {
 //   io::stdin().read_line(&mut name).expect("Error reading input");
 // println!("Welcome {}, to our app.", name.trim())
 
+let mut how_many = String::new();
+println!("\nHow many random numbers do you want to guess?");
+io::stdin().read_line(&mut how_many).expect("Error reading data from user input");
+let mut num_guesses:u32 = how_many.trim().parse().expect("Error parsing data");
+
 loop{
-     println!("\nHey guess a number from 1-10: ");
+    if num_guesses <= 0 {break;}
+
+    println!("\nHey guess a number from 1-10: ");
     let mut guess = String::new();
     let correct_num  = rand::rng().random_range(1..=10);
 
@@ -26,17 +33,18 @@ loop{
     };
 
     println!("The correct number is {}",correct_num);
-
+    
     // let winner = if correct_num == parsed_num {String::from("User")} else {String::from("PC")};
 
     let winner = match guess.cmp(&correct_num) {
-    Ordering::Equal => String::from("User"),
-    _ => String::from("PC")
+        Ordering::Equal => String::from("User"),
+        _ => String::from("PC")
     };
     if winner == "User" {
-    break;
+        break;
     }
-    println!("The winner is {}", winner)
-
-    };
+    println!("The winner is {}", winner);
+    
+    num_guesses -=  1;
+};
 }
