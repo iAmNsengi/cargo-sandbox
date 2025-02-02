6ec9f2487298b49f8f0c49a0a929831f8d4d70ef
iAmNsengi <iamnsengi@icloud.com>
2025-02-02 12:29:18 +0200

    ft: add sign token method

--- backend/src/routes/gateRoutes.js
+++ backend/src/routes/gateRoutes.js
@@ -1,6 +1,13 @@
 import express from "express";
+import { protect, restrictTo } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 // protect all routes
 router.use(protect);
+
+// routes only accessible to coaches and admin
+router.use(restrictTo("coach", "admin"));
+
+// routes
+router.post("/schedule", scheduleGateInterview);
