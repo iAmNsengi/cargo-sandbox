0bcb50a91ffe2b5b1b6c43140b7c4246326bcb57
iAmNsengi <iamnsengi@icloud.com>
2025-02-05 13:17:43 +0200

    ft: add gates routing

--- backend/src/routes/gateRoutes.js
+++ backend/src/routes/gateRoutes.js
@@ -3,11 +3,16 @@ import { protect, restrictTo } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-// protect all routes
+// protect all routes with auth
 router.use(protect);
 
 // routes only accessible to coaches and admin
 router.use(restrictTo("coach", "admin"));
 
-// routes
+router.route("").get(allGates).post(createGate);
+
+router.route(":/id").get(getGateById).patch(editGate).delete(deleteGate);
+
 router.post("/schedule", scheduleGateInterview);
+
+export default router;
