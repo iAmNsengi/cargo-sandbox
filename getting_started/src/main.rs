7074cdbab594be6ae5c23e50e8f50cfc4569732f
iAmNsengi <iamnsengi@icloud.com>
2025-02-20 22:33:43 +0200

    ft: guessing game v2

---
+++ getting_started/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+use rand::Rng;
+
+
+fn main() {
+    loop {
+        let mut number = String::new();
+        println!("Enter any number: ");
+        io::stdin().read_line(&mut number).expect("Error reading number from user");
+        
+        let  parsed_number : u32 = match number.trim().parse() {
+            Ok(val) => val,
+            _=> {
+                println!("Error parsing number, enter a valid number.");
+                return;
+    }
+};
+let random_num : u32 = rand::rng().random_range(0..=10);
+
+if parsed_number == random_num {
+    println!("Congratulations you won!");
+    break;
+}else {
+    println!("Sorry you lost!, correct number was: {}.",random_num)
+}
+}
+}
