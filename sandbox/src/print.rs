4c574e2ad640ec2ded78ded42623c2979e52e331
iAmNsengi <iamnsengi@icloud.com>
2025-03-18 12:21:37 +0200

    ft: add placeholder for debug traits

--- sandbox/src/print.rs
+++ sandbox/src/print.rs
@@ -1,19 +1,27 @@
-
-pub fn run(){
+pub fn run() {
     // print to console
     println!("Hello, from print file");
 
     // formatting
     println!("A number: {}", 1);
-    println!("{} is from {}", "Nsengi","Kigali");
+    println!("{} is from {}", "Nsengi", "Kigali");
 
     //positional arguments
-    println!("{0} is from {1} and {0} likes to {2}", "Nsengi","Kigali","Code");
+    println!(
+        "{0} is from {1} and {0} likes to {2}",
+        "Nsengi", "Kigali", "Code"
+    );
 
     // named arguments
-    println!("{name} likes to play {activity}", name="Nsengi", activity="Piano");
-
+    println!(
+        "{name} likes to play {activity}",
+        name = "Nsengi",
+        activity = "Piano"
+    );
 
     // placeholder traints
-    println!("Binary '{:b}', Hexadecimal '{:x}' Octal '{:o}'", 10,10,10)
+    println!("Binary '{:b}', Hexadecimal '{:x}' Octal '{:o}'", 10, 10, 10);
+
+    // placeholder for debug trait
+    println!("{:?}", (1, true, "hello"))
 }
