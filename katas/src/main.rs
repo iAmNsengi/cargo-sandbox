a056195bc3ff2be7128febc6703acfc557047005
iAmNsengi <iamnsengi@icloud.com>
2025-03-24 17:59:41 +0200

    ft: add user struct

--- katas/src/main.rs
+++ katas/src/main.rs
@@ -2,6 +2,13 @@ mod fibonacci;
 mod is_even;
 mod string_length;
 
+pub struct User {
+    username: String,
+    password: String,
+    email: String,
+    sign_in_count: i32,
+}
+
 fn main() {
     let num = 11;
     if is_even::is_even(num) {
@@ -18,5 +25,17 @@ fn main() {
     println!(
         "Get length of a string: {}",
         string_length::get_string_length("iAmNsengi")
-    )
+    );
+
+    let user = User {
+        username: String::from("nsengi"),
+        password: String::from("nsengi"),
+        email: String::from("nsengi"),
+        sign_in_count: 1,
+    };
+
+    println!("User info {:?}", user.username);
+    println!("User password {:?}", user.password);
+    println!("User email {:?}", user.email);
+    println!("User sign in count {:?}", user.sign_in_count);
 }
