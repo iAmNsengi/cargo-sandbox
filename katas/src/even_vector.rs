d4fdc5104292915f02b7609f2ee6507f8739ff9f
iAmNsengi <iamnsengi@icloud.com>
2025-04-07 19:42:28 +0200

    ft: use retain to filter out the vector:

--- katas/src/even_vector.rs
+++ katas/src/even_vector.rs
@@ -1,10 +1,3 @@
-pub fn get_even_from_vector(vec: Vec<i32>)-> Vec<i32>{
-    let mut even_vectors = Vec::new();
-
-    for item in vec{
-        if item %2 == 0{
-            even_vectors.push(item);
-        }
-    }
-    even_vectors
+pub fn get_even_from_vector(vec: &mut Vec<i32>){
+    vec.retain(|&item| item %2 == 0);
 }
--- katas/src/main.rs
+++ katas/src/main.rs
@@ -30,8 +30,10 @@ enum Shape {
 }
 
 fn main() {
-    let vec = Vec::from([1,2,4,5,6]);
-    println!("{:?}",even_vector::get_even_from_vector(vec));
+    let mut vec = Vec::from([1,2,4,5,6]);
+    even_vector::get_even_from_vector(&mut vec);
+    println!("{:?}",vec);
+
     let num = 11;
     if is_even::is_even(num) {
         println!("Number {num} is even ");
